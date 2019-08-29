aea506f980d66511553e3c0dbe02452829775f41
Maria Verdonck <verdonck.marie@gmail.com>
2019-08-29 16:51:51 +0200

    DB: sql event to cleanup rows

--- scripts/db-max-rows-log.sql
+++ scripts/db-max-rows-log.sql
@@ -9,3 +9,26 @@ FOR EACH ROW BEGIN
     END IF;
 END$
 delimiter ;
+
+SET GLOBAL event_scheduler = ON;
+
+DELIMITER $
+DROP EVENT IF EXISTS log_rows_cleanup_check$
+CREATE EVENT log_rows_cleanup_check
+ON SCHEDULE EVERY 1 HOUR
+STARTS '2019-08-25 00:00:00'
+DO
+ CALL log_rows_cleanup();
+$
+DROP PROCEDURE IF EXISTS log_rows_cleaup$
+CREATE PROCEDURE log_rows_cleanup()
+BEGIN
+	DECLARE nr_rows_to_delete INT DEFAULT 0;
+	SET nr_rows_to_delete = (SELECT COUNT(*) FROM log) - 80;
+	
+	DELETE FROM log
+	ORDER BY log_id ASC
+	LIMIT nr_rows_to_delete;
+END$
+
+DELIMITER ;
